36b0ec7370efaf12928797b82a860c860449ff37
kevit-siddharth-singh <siddharth.singh@kevit.io>
2026-06-12 16:08:49 +0530

    style/formatting

--- src/modules/projects/dto/update-project.dto.ts
+++ src/modules/projects/dto/update-project.dto.ts
@@ -1,10 +1,5 @@
 import { ApiPropertyOptional } from '@nestjs/swagger';
-import {
-  IsArray,
-  IsMongoId,
-  IsOptional,
-  IsString,
-} from 'class-validator';
+import { IsArray, IsMongoId, IsOptional, IsString } from 'class-validator';
 
 export class UpdateProjectDto {
   @ApiPropertyOptional({ example: 'Updated Project Title' })
